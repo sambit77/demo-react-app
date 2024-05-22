2fd3c43f2125d9b570ac18be4d5e04bf7fed870e
Sambit Pradhan <sambitgulu@gmail.com>
2024-05-23 03:42:02 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from "./User";
+import About from "./About";
+import Home from "./Home";
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+
+      <nav>
+            <Link to="/"> Home </Link>
+            <Link to="about">About</Link>
+      </nav>
+
+      <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+      </Routes>
+
+      <div>
+        <User name = "Sambit" email="sambitgulu@gmail.com"/>
+      </div>
+
+      <div>
+              <User name = "Msdika" email="msd7@gmail.com"/>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
